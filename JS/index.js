03e6983e10668ae0a261d3d338d4e92fd6734495
kohinoor123shrestha <kohinoorshrestha0@gmail.com>
2025-03-02 20:44:10 +0545

    Basic JS variables and interactions

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+const person1=" Kohinoor shrestha";
+const person2="Kamala Rawal";
+let roll=1214;
+roll=1225;
+console.log(roll);
+console.log(person1,person2);
+console.log(person1+","+person2);
+console.log({person1});
+console.log({person2});
+
+alert("This will display an alert message");
+prompt("What is your name?");
+confirm("Are you 18 years or above?");
+
+const question1=prompt ("whats your age?");
+const question2=prompt("What is your roll number?");
+console.log(question1,question2);
+
+//print age and roll through alert
+alert(" Your age is " +question1+ "and"  +question2+ "is your roll number." ); //+ for string concatenation
